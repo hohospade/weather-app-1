666883b4b415dc38c1df1b7fa5c48eb203dc5bd9
hohospade <vigne6@gmail.com>
2018-07-02 21:31:33 -0700

    update4

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,4 @@
 import prod from './prod';
 import dev from './dev';
 
-export default process.env.NODE_ENV === 'production' ? prod : dev
+export default process.env.NODE_ENV === 'production' ? prod : dev;
