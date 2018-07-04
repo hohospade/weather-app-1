c01dadee3027bd7056285532f445dab0e8587780
hohospade <vigne6@gmail.com>
2018-07-03 17:28:27 -0700

    update6

--- src/components/SearchHistory/index.js
+++ src/components/SearchHistory/index.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import SearchHistory from './SearchHistory';
-
+//File taken from budget tracker as foundation to update for this new project.
 function mapStoreToProps(store) {
     return {
         searchItems: store.searchHistory.searchItems
--- src/js/rootStore.js
+++ src/js/rootStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './rootReducer';
 import promiseMiddleware from 'redux-promise-middleware';
-
+//File taken from budget tracker and modified to work with weather app.
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootStore = createStore(rootReducer, composeEnhancers(applyMiddleware(promiseMiddleware())));
 
