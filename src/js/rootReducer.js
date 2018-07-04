183d4ff2271819a108556e3ce7426f621ebfad95
hohospade <vigne6@gmail.com>
2018-07-03 18:22:05 -0700

    test

--- src/js/rootReducer.js
+++ src/js/rootReducer.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import SearchFieldReducer from '../components/SearchField/searchFieldReducer';
-import SearchHistoryReducer from '../components/SearchHistory/searchHistoryReducer';
+// import SearchHistoryReducer from '../components/SearchHistory/searchHistoryReducer';
 //format taken from budget tracker and update with current paths and changes.
 const rootReducer = combineReducers({
   searchField: SearchFieldReducer,
-  searchHistory: SearchHistoryReducer,
+  // searchHistory: SearchHistoryReducer,
 });
 
 export default rootReducer;
