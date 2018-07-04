fd95802d492772e54904e3f846407eb056386876
hohospade <vigne6@gmail.com>
2018-07-03 19:24:18 -0700

    heroku plz

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
+  console.log(process.env.WEATHER_APP_API)
   module.exports = require('./prod')
 } else {
   module.exports = require('./dev');
--- src/config/prod.js
+++ src/config/prod.js
@@ -1,3 +1,3 @@
-export default {
+module.exports = {
   weatherAppApi: process.env.WEATHER_APP_API
 }
