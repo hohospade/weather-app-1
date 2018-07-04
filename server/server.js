016c94b5063786517cc46bd1cc8627da20d19732
hohospade <vigne6@gmail.com>
2018-07-03 18:06:04 -0700

    path

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
 const axios = require('axios');
 require('dotenv').config();
@@ -7,7 +8,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static('dist'));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 module.exports = app;
