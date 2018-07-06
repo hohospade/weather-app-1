8bf5e14d8926b515c4ffd73f612b2a117cfce09b
hohospade <vigne6@gmail.com>
2018-07-05 17:28:36 -0700

    Fixes casing for github

--- src/js/rootReducer.js
+++ src/js/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import SearchFieldReducer from '../components/SearchField/searchFieldReducer';
-import SearchHistoryReducer from '../components/SearchHistory/searchHistoryReducer';
+import SearchHistoryReducer from '../components/SearchHistory/SearchHistoryReducer';
 //format taken from budget tracker and update with current paths and changes.
 const rootReducer = combineReducers({
   searchField: SearchFieldReducer,
