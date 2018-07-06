deda7134bf698b6d511320ee4829ce7d6df46eeb
hohospade <vigne6@gmail.com>
2018-07-05 17:33:10 -0700

    Fixes naming issue with searchHistoryReducer

--- src/js/rootReducer.js
+++ src/js/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import SearchFieldReducer from '../components/SearchField/searchFieldReducer';
-import SearchHistoryReducer from '../components/SearchHistory/SearchHistoryReducer';
+import SearchHistoryReducer from '../components/SearchHistory/searchHistoryReducer';
 //format taken from budget tracker and update with current paths and changes.
 const rootReducer = combineReducers({
   searchField: SearchFieldReducer,
